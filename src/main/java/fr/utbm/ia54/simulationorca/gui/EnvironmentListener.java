2d768b437b65a8424aeaf6b06c8979b239fdf5a2
camille mougin <mougin.camille@gmail.com>
2014-11-13 15:27:10 +0100

    Refactor des packages sarl + classes event/skill/capacity

--- src/main/java/fr/utbm/ia54/simulationorca/gui/EnvironmentListener.java
+++ src/main/java/fr/utbm/ia54/simulationorca/gui/EnvironmentListener.java
@@ -1,9 +1,10 @@
 package fr.utbm.ia54.simulationorca.gui;
 
+import java.util.EventListener;
 import java.util.Set;
 
 import fr.utbm.ia54.simulationorca.environmentmodel.EnvironmentObject;
 
-public interface EnvironmentListener {
+public interface EnvironmentListener extends EventListener {
 	void updateGraphics(Set<EnvironmentObject> environmentObjects);
 }
