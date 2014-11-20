49fd9d5e0641a8b5baffa931ef4bc4cea031d314
pitau07 <julian.decreuse@utbm.fr>
2014-11-20 14:43:24 +0100

    utilise le parser et ouvre la JFrame

--- src/test/java/fr/utbm/ia54/projet/SimulationORCA/XMLParserTest.java
+++ src/test/java/fr/utbm/ia54/projet/SimulationORCA/XMLParserTest.java
@@ -8,15 +8,10 @@ public class XMLParserTest extends TestCase {
 	
 	public void testApp(){
 		XMLParser xmlParser = new XMLParser("./environment_structure.xml");	
-		xmlParser.loadXMLDocument();
 
 		System.out.println("Taille liste pos_init : " + xmlParser.getListInitialPositionPedestrians().size());
 		System.out.println("Taille liste pos_final : " + xmlParser.getListFinalPositionPedestrians().size());
 		System.out.println("Taille liste obstacles : " + xmlParser.getListObstacles().size());
-		
-		for(Obstacle obs : xmlParser.getListObstacles()){
-			System.out.println("Obstacle " + obs.getPosA().getX());
-		}
 		
 		for(Obstacle obs : xmlParser.getListObstacles()){
 			System.out.println("Obstacle " + obs.getPosA().getX());
