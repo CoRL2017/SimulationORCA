dd9f493568b898743049fea4add2a2dafbbcdc10
pitau07 <julian.decreuse@utbm.fr>
2014-12-01 22:03:01 +0100

    ouragan galland mais bug si decommente ligne dans InfluenceEvent

--- src/main/java/fr/utbm/ia54/simulationorca/environmentmodel/Obstacle.java
+++ src/main/java/fr/utbm/ia54/simulationorca/environmentmodel/Obstacle.java
@@ -20,4 +20,33 @@ public final class Obstacle extends EnvironmentObject{
 		return posB;
 	}
 	
+	private static float projectsPointOnLine(float px, float py, float s1x, float s1y, float s2x, float s2y) {
+		float r_numerator = (px-s1x)*(s2x-s1x) + (py-s1y)*(s2y-s1y);
+		float r_denomenator = (s2x-s1x)*(s2x-s1x) + (s2y-s1y)*(s2y-s1y);
+		return r_numerator / r_denomenator;
+	}
+	
+	private static float clamp(float v, float min, float max) {
+		if (min<max) {
+			if (v<min) return min;
+			if (v>max) return max;
+		}
+		else {
+			if (v>min) return min;
+			if (v<max) return max;
+		}
+		return v;
+	}
+
+	public float distanceTo(Position p) {
+		float ratio = projectsPointOnLine(p.getX(), p.getY(),
+				getPosA().getX(), getPosA().getY(),
+				getPosB().getX(), getPosB().getY());
+		ratio = clamp(ratio, 0f, 1f);
+		float vx = ratio * (getPosB().getX() - getPosA().getX());
+		float vy = ratio * (getPosB().getY() - getPosA().getY());
+		return Math.abs(getPosA().getX() + vx - p.getX())
+				+ Math.abs(getPosA().getY() + vy - p.getY());
+	}
+	
 }
